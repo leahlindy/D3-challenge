89b2920654e47c1683b64f8ec3b0d826aef93b22
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-05-31 14:48:42 -0400

    additional graphs

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -1,3 +1,4 @@
+//General Layout dimensions for all graphs
 // Define SVG area dimensions
 var svgWidth = 500;
 var svgHeight = 500;
@@ -8,38 +9,27 @@ right: 60,
 bottom: 60,
 left: 60
 };
-
 // Define dimensions of the chart area
 var chartWidth = svgWidth - margin.left - margin.right;
 var chartHeight = svgHeight - margin.top - margin.bottom;
 
-// console.log(margin.left);
-// console.log(chartHeight);
-//Set SVG element with d3 within the html body (width and height attributes included)
-// Select body, append SVG area to it, and set its dimensions
-var svg = d3.select("#scatter")
-.append("svg")
-.attr("width", svgWidth)
-.attr("height", svgHeight);
-var svg2 = d3.select("#scatter")
-.append("svg")
-.attr("width", svgWidth)
-.attr("height", svgHeight);
-
-////Add a group area within the svg and set margins to top left of group
-// Append a group area, then set its margins
-var chartGroup = svg.append("g")
-.attr("transform", `translate(${margin.left}, ${margin.top})`);
-var chartGroup2= svg2.append("g")
-.attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-//create functions to simplify process & display code nicely:
 //--------------------------///
+//create functions to simplify process & display code nicely:
 // 1. healthcare v poverty//
 function healthcareVSpoverty(){
+    //Set SVG element with d3 within the html body (width and height attributes included)
+    // Select body, append SVG area to it, and set its dimensions
+    var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+    
+    // Append a group area, then set its margins
+    var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    
     //Load the data with d3.csv from data.csv file
     d3.csv("data.csv").then(function(dataOutcomes){
-        console.log(dataOutcomes);
         
         //Type cast data for variables needed in scatter (poverty, healthcate, smokes, age)
         dataOutcomes.forEach(function(data) {
@@ -141,10 +131,22 @@ function healthcareVSpoverty(){
             }).catch(function(error) {
                 console.log(error)});
 }
-
 //--------------------------///
 // 2. smokers v age(x)//
 function smokerVSage(){
+    //Set SVG element with d3 within the html body (width and height attributes included)
+    // Select body, append SVG area to it, and set its dimensions
+    var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+    ////Add a group area within the svg and set margins to top left of group
+    // Append a group area, then set its margins
+    var chartGroup= svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // read csv data
     d3.csv("data.csv").then(function(dataOutcomes){
         
         dataOutcomes.forEach(function(data) {
@@ -152,29 +154,29 @@ function smokerVSage(){
             data.age = +data.age;
             });
 
-        var x2= d3.scaleLinear()
+        var x= d3.scaleLinear()
         .domain([25, d3.max(dataOutcomes, d => d.age)])
         .range([0, chartWidth]);
         
-        var y2 = d3.scaleLinear()
+        var y = d3.scaleLinear()
         .domain([5, d3.max(dataOutcomes, d => d.smokes)+3])
         .range([chartHeight, 0]);
     
         // Create two new functions passing our scales in as arguments
         // These will be used to create the chart's axes
-        var bottomAxis = d3.axisBottom(x2);
-        var leftAxis = d3.axisLeft(y2);
+        var bottomAxis = d3.axisBottom(x);
+        var leftAxis = d3.axisLeft(y);
     
         // append the axis to chart:
-        chartGroup2.append("g")
+        chartGroup.append("g")
         .call(leftAxis);
     
-        chartGroup2.append("g")
+        chartGroup.append("g")
         .attr("transform", `translate(0, ${chartHeight})`)
         .call(bottomAxis);
     
         // Create and place the "blocks" to hold the circles and text 
-        var circles = chartGroup2.selectAll("g")
+        var circles = chartGroup.selectAll("g")
         .data(dataOutcomes)
         .enter()
         .append("g");
@@ -184,8 +186,8 @@ function smokerVSage(){
         var circlePlot= circles
         .append("circle")
         .attr("class", "dot")
-            .attr("cx", d=> x2(d.age))
-            .attr("cy", d=> y2(d.smokes))
+            .attr("cx", d=> x(d.age))
+            .attr("cy", d=> y(d.smokes))
             .attr("r", 9)
             .attr("opacity", .5)
             .style("stroke", "red")
@@ -193,29 +195,29 @@ function smokerVSage(){
         
         //Create text for each circle group
         circles.append("text")
-        .attr("dx", d=>x2(d.age))
-        .attr("dy", d=>y2(d.smokes)+2.5)
+        .attr("dx", d=>x(d.age))
+        .attr("dy", d=>y(d.smokes)+2.5)
         .attr("text-anchor", "middle") 
         .style("font-size", "10px") 
         .style("fill", "black")
         .text(d=>d.abbr);
     
         // add title/labels to chart
-        chartGroup2.append("text")
+        chartGroup.append("text")
             .attr("x", (chartWidth / 2))             
             .attr("y", 0 - (margin.top / 2))
             .attr("text-anchor", "middle")  
             .style("font-size", "16px") 
             .text("Age vs Smokers");
         
-        chartGroup2.append("text")
+        chartGroup.append("text")
             .attr("y", 0-(margin.left / 2))
             .attr("transform", `translate(0, ${chartHeight/2}) rotate(-90)`)
             .attr("text-anchor", "middle")  
             .style("font-size", "14px") 
             .text("Smokers (%)");
         
-        chartGroup2.append("text")
+        chartGroup.append("text")
             .attr("x", (chartWidth / 2))             
             .attr("y", (chartHeight+margin.bottom/2))
             .attr("text-anchor", "middle")  
@@ -248,8 +250,243 @@ function smokerVSage(){
             console.log(error)});;
 
 }
+// 3. Obesity v poverty//
+function obesityVSpoverty(){
+    //Set SVG element with d3 within the html body (width and height attributes included)
+    // Select body, append SVG area to it, and set its dimensions
+    var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+    
+    // Append a group area, then set its margins
+    var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    
+    //Load the data with d3.csv from data.csv file
+    d3.csv("data.csv").then(function(dataOutcomes){
+        console.log(dataOutcomes);
+        //Type cast data for variables needed in scatter (poverty, healthcate, smokes, age)
+        dataOutcomes.forEach(function(data) {
+            data.poverty = +data.poverty;
+            data.obesity = +data.obesity;
+            });
+        
+            
+            // Create a linear scale for the horizontal/vertical axis.
+            var x= d3.scaleLinear()
+            .domain([0, d3.max(dataOutcomes, d => d.poverty)+1])
+            .range([0, chartWidth]);
+            
+            var y = d3.scaleLinear()
+            .domain([0, d3.max(dataOutcomes, d => d.obesity)+2])
+            .range([chartHeight, 0]);
+        
+            // Create two new functions passing our scales in as arguments
+            // These will be used to create the chart's axes
+            var bottomAxis = d3.axisBottom(x);
+            var leftAxis = d3.axisLeft(y);
+        
+            // append the axis to chart:
+            chartGroup.append("g")
+            .call(leftAxis);
+        
+            chartGroup.append("g")
+            .attr("transform", `translate(0, ${chartHeight})`)
+            .call(bottomAxis);
+        
+            // Create and place the "blocks" to hold the circles and text 
+            var circles = chartGroup.selectAll("g")
+            .data(dataOutcomes)
+            .enter()
+            .append("g");
+            // .attr("transform", function(d){return `translate(${d.poverty},${d.healthcare}`})
+        
+            // Configure a plot function to plot scatter points 
+            var circlePlot= circles
+            .append("circle")
+            .attr("class", "dot")
+                .attr("cx", d=> x(d.poverty))
+                .attr("cy", d=> y(d.obesity))
+                .attr("r", 9)
+                .attr("opacity", .5)
+                .style("stroke", "red")
+                .style("fill", "#69b3a2");
+            
+            //Create text for each circle group
+            circles.append("text")
+            .attr("dx", d=>x(d.poverty))
+            .attr("dy", d=>y(d.obesity)+2.5)
+            .attr("text-anchor", "middle") 
+            .style("font-size", "10px") 
+            .style("fill", "black")
+            .text(d=>d.abbr);
+        
+            // add title/labels to chart
+            chartGroup.append("text")
+                .attr("x", (chartWidth / 2))             
+                .attr("y", 0 - (margin.top / 2))
+                .attr("text-anchor", "middle")  
+                .style("font-size", "16px") 
+                .text("Obesity vs Poverty");
+            
+            chartGroup.append("text")
+                .attr("y", 0-(margin.left / 2))
+                .attr("transform", `translate(0, ${chartHeight/2}) rotate(-90)`)
+                .attr("text-anchor", "middle")  
+                .style("font-size", "14px") 
+                .text("Obesity (%)");
+            
+            chartGroup.append("text")
+                .attr("x", (chartWidth / 2))             
+                .attr("y", (chartHeight+margin.bottom/2))
+                .attr("text-anchor", "middle")  
+                .style("font-size", "14px") 
+                .text("In Poverty (%)");
+            
+            // Step 1: Append tooltip div
+            var toolTip1 = d3.tip()
+            .attr("class", "d3-tip")
+            .html(function(d) {
+                return (`<p class='font-weight-bold'>${d.state}: </p>
+                <p>Obesity: % ${(d.obesity)}<br> Poverty: ${(d.poverty)}</p>`);
+            });
+            // Step 2: Create the tooltip in chartGroup.
+            circlePlot.call(toolTip1);
+        
+            // Step 3: Create "mouseover" event listener to display tooltip
+            circles.on("mouseover", function(d) {
+                toolTip1.style("cursor", "cell");
+                toolTip1.show(d, this);
+                })
+            // Step 4: Create "mouseout" event listener to hide tooltip
+                .on("mouseout", function(d) {
+                    toolTip1.style("cursor", "default");
+                    toolTip1.hide(d);
+                    }) 
+            }).catch(function(error) {
+                console.log(error)});
+}
+
+// 4. Smokes v poverty//
+function smokesVSpoverty(){
+    //Set SVG element with d3 within the html body (width and height attributes included)
+    // Select body, append SVG area to it, and set its dimensions
+    var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+    
+    // Append a group area, then set its margins
+    var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    
+    //Load the data with d3.csv from data.csv file
+    d3.csv("data.csv").then(function(dataOutcomes){
+        console.log(dataOutcomes);
+        //Type cast data for variables needed in scatter (poverty, healthcate, smokes, age)
+        dataOutcomes.forEach(function(data) {
+            data.poverty = +data.poverty;
+            data.smokes = +data.smokes;
+            });
+        
+            // Create a linear scale for the horizontal/vertical axis.
+            var x= d3.scaleLinear()
+            .domain([0, d3.max(dataOutcomes, d => d.poverty)+1])
+            .range([0, chartWidth]);
+            
+            var y = d3.scaleLinear()
+            .domain([0, d3.max(dataOutcomes, d => d.smokes)+2])
+            .range([chartHeight, 0]);
+        
+            // Create two new functions passing our scales in as arguments
+            // These will be used to create the chart's axes
+            var bottomAxis = d3.axisBottom(x);
+            var leftAxis = d3.axisLeft(y);
+        
+            // append the axis to chart:
+            chartGroup.append("g")
+            .call(leftAxis);
+        
+            chartGroup.append("g")
+            .attr("transform", `translate(0, ${chartHeight})`)
+            .call(bottomAxis);
+        
+            // Create and place the "blocks" to hold the circles and text 
+            var circles = chartGroup.selectAll("g")
+            .data(dataOutcomes)
+            .enter()
+            .append("g");
+            // .attr("transform", function(d){return `translate(${d.poverty},${d.healthcare}`})
+        
+            // Configure a plot function to plot scatter points 
+            var circlePlot= circles
+            .append("circle")
+            .attr("class", "dot")
+                .attr("cx", d=> x(d.poverty))
+                .attr("cy", d=> y(d.smokes))
+                .attr("r", 9)
+                .attr("opacity", .5)
+                .style("stroke", "red")
+                .style("fill", "#69b3a2");
+            
+            //Create text for each circle group
+            circles.append("text")
+            .attr("dx", d=>x(d.poverty))
+            .attr("dy", d=>y(d.smokes)+2.5)
+            .attr("text-anchor", "middle") 
+            .style("font-size", "10px") 
+            .style("fill", "black")
+            .text(d=>d.abbr);
+        
+            // add title/labels to chart
+            chartGroup.append("text")
+                .attr("x", (chartWidth / 2))             
+                .attr("y", 0 - (margin.top / 2))
+                .attr("text-anchor", "middle")  
+                .style("font-size", "16px") 
+                .text("Smokes vs Poverty");
+            
+            chartGroup.append("text")
+                .attr("y", 0-(margin.left / 2))
+                .attr("transform", `translate(0, ${chartHeight/2}) rotate(-90)`)
+                .attr("text-anchor", "middle")  
+                .style("font-size", "14px") 
+                .text("Smokes (%)");
+            
+            chartGroup.append("text")
+                .attr("x", (chartWidth / 2))             
+                .attr("y", (chartHeight+margin.bottom/2))
+                .attr("text-anchor", "middle")  
+                .style("font-size", "14px") 
+                .text("In Poverty (%)");
+            
+            // Step 1: Append tooltip div
+            var toolTip1 = d3.tip()
+            .attr("class", "d3-tip")
+            .html(function(d) {
+                return (`<p class='font-weight-bold'>${d.state}: </p>
+                <p>Smokers: % ${(d.smokes)}<br> Poverty: ${(d.poverty)}</p>`);
+            });
+            // Step 2: Create the tooltip in chartGroup.
+            circlePlot.call(toolTip1);
+        
+            // Step 3: Create "mouseover" event listener to display tooltip
+            circles.on("mouseover", function(d) {
+                toolTip1.style("cursor", "cell");
+                toolTip1.show(d, this);
+                })
+            // Step 4: Create "mouseout" event listener to hide tooltip
+                .on("mouseout", function(d) {
+                    toolTip1.style("cursor", "default");
+                    toolTip1.hide(d);
+                    }) 
+            }).catch(function(error) {
+                console.log(error)});
+}
 
 healthcareVSpoverty();
 smokerVSage();
-
+obesityVSpoverty();
+smokesVSpoverty();
     
